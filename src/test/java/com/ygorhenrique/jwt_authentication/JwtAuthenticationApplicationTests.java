760aa6c93fe1537de59195fd1ceeba7c45e1e23d
ygor-h <ygorlh01@gmail.com>
2026-01-07 17:03:07 -0300

    initial commit

---
+++ src/test/java/com/ygorhenrique/jwt_authentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygorhenrique.jwt_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
