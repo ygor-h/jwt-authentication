a41f4edf5b32358eaeac30e6e1c9ebbba14fe725
ygor-h <ygorlh01@gmail.com>
2026-01-07 21:47:06 -0300

    feat: add UserRepository interface

---
+++ src/main/java/com/ygorhenrique/jwt_authentication/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ygorhenrique.jwt_authentication.repository;
+
+import com.ygorhenrique.jwt_authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByLogin(String login);
+}
